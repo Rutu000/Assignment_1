91d904ca7356040e06a57fb82b2668d0a822b5f4
Rutu000 <155481766+Rutu000@users.noreply.github.com>
2025-08-05 23:32:18 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,14 @@
+a=input("Enter the first number: ")
+print(a)
+a=float(a)
+b=input("Enter the second number: ")
+print(b)
+b=float(b)
+Addition = a+b
+print("Addition: ",Addition)
+Subtraction = a-b
+print("Subtraction: ",Subtraction)
+Multiplication = a*b
+print("Multiplication: ",Multiplication)
+Division = a/b
+print("Division: ",Division)
---
+++ Task2.py
@@ -0,0 +1,4 @@
+first_n = input("Enter the first name: ")
+last_n = input("Enter the last name: ")
+full_name = first_n+" "+last_n
+print("Hello,", full_name + "! Welcome to the Python Programming Course!")
